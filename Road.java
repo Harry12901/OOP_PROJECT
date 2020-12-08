013fec3a5c6c530c9dbb19415444cc26a8f80ea4
Harini Sivakumar <63298200+Harry12901@users.noreply.github.com>
2020-12-09 04:24:28 +0530

    Create Road.java

---
+++ Road.java
@@ -0,0 +1,12 @@
+public class Road {
+	    int RoadId;
+	    String RoadName;
+	    double Length;
+
+	    public Road(int roadId, String roadName, double length) {
+	        RoadId = roadId;
+	        RoadName = roadName;
+	        Length = length;
+	    }
+
+} 
