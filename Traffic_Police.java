ef74b0b6b99edf5f4913263c7b307cc406f5aab8
Harini Sivakumar <63298200+Harry12901@users.noreply.github.com>
2020-12-09 04:25:05 +0530

    Create Traffic_Police.java

---
+++ Traffic_Police.java
@@ -0,0 +1,29 @@
+public class Traffic_Police {
+	int PoliceID;
+    public String Name;     //police-name
+    private int Salary;
+    private int Age;
+    public int YearsOfExperience;
+    private String Address;
+
+    public Traffic_Police(int policeID, String name, int salary, int age, int yearsOfExperience, String address) {
+        PoliceID = policeID;
+        Name = name;
+        Salary = salary;
+        Age = age;
+        YearsOfExperience = yearsOfExperience;
+        Address = address;
+    }
+    public int getSalary()
+    {
+    	return Salary;
+    }
+    public int getAge()
+    {
+    	return Age;
+    }
+    public String getAddress()
+    {
+    	return Address;
+    }
+}
